87938ada45f986dd2e632a2fc59b1cdf05d055c4
Jeba-tahsin <jebatahsin500@.com>
2021-10-29 15:47:05 +0600

    jeba

---
+++ src/Pages/Shared/Footer/Footer.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <div>
+            <div className='mt-5 footer' style={{ padding:'10px'}}>
+            <h2>CONTACT US</h2>
+            <p>Road-2/A East Shibgonh Sylhet-300, Bangladesh</p>
+            <p>Cell:(+880) 0723 161 343</p>
+            <p>helpme@donation.com</p>
+        </div>
+        </div>
+    );
+};
+
+export default Footer;
